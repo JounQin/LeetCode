ae286d72013dcb9aa8d091ee16e08ed52efe8385
Jiaren Liu <jrliu@alauda.io>
2018-05-06 14:51:12 +0800

    better solution for two-sum

--- 001.two-sum/index.js
+++ 001.two-sum/index.js
@@ -1,10 +1,10 @@
+// @ts-check
 /**
- * @ts-check
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-export const twoSum = (nums, target) => {
+export const twoSum1 = (nums, target) => {
   const len = nums.length
   for (let i = 0; i < len - 1; i++) {
     for (let j = i + 1; j < len; j++) {
@@ -12,5 +12,23 @@ export const twoSum = (nums, target) => {
         return [i, j]
       }
     }
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+export const twoSum2 = (nums, target) => {
+  const len = nums.length
+  const map = {}
+  for (let i = 0; i < len; i++) {
+    const curr = nums[i]
+    const j = map[target - curr]
+    if (j != null) {
+      return [j, i]
+    }
+    map[curr] = i
   }
 }
--- 001.two-sum/index.spec.js
+++ 001.two-sum/index.spec.js
@@ -1,9 +1,16 @@
 // @ts-check
-import { twoSum } from '.'
+import { twoSum1, twoSum2 } from '.'
 
-test('twoSum', () => {
-  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
-  expect(twoSum([2, 7, 11, 15], 13)).toEqual([0, 2])
-  expect(twoSum([2, 7, 11, 15], 22)).toEqual([1, 3])
-  expect(twoSum([2, 7, 11, 15], 26)).toEqual([2, 3])
+test('two-sum-1', () => {
+  expect(twoSum1([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum1([2, 7, 11, 15], 13)).toEqual([0, 2])
+  expect(twoSum1([2, 7, 11, 15], 22)).toEqual([1, 3])
+  expect(twoSum1([2, 7, 11, 15], 26)).toEqual([2, 3])
+})
+
+test('two-sum-2', () => {
+  expect(twoSum2([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum2([2, 7, 11, 15], 13)).toEqual([0, 2])
+  expect(twoSum2([2, 7, 11, 15], 22)).toEqual([1, 3])
+  expect(twoSum2([2, 7, 11, 15], 26)).toEqual([2, 3])
 })
